8927940faf6621b2f8e8952d555170f83f48322a
Rafael Calunga <rafaelcalunga@gmail.com>
2021-07-01 14:34:16 -0400

    Initial Commit

---
+++ BatalhaNaval/BatalhaNavalApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatalhaNavalApp.swift
+//  BatalhaNaval
+//
+//  Created by Rafael Calunga on 2021-07-01.
+//
+
+import SwiftUI
+
+@main
+struct BatalhaNavalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
